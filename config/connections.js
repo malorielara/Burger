b386a68eda1d9c5db6cc814a7a52ecf8ed939f43
Malorie Lara <malorielara@users.noreply.github.com>
2018-11-11 14:49:55 -0600

    mas

---
+++ config/connections.js
@@ -0,0 +1,20 @@
+// set up mysql 
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    port: 8080,
+    host: "localhost",
+    user: "root",
+    database: "burgers_db"
+});
+
+// make the connection
+connection.connect(function(err) {
+    if(err) {
+        console.log("error connecting:" + err.stack);
+        return;
+    }
+    console.log("Connected as id: " + connection.threadId)
+});
+
+module.exports = connection;
