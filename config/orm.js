610e9a92abbb25cad8a3cd719a37390fb69aab6c
Malorie Lara <malorielara@users.noreply.github.com>
2018-11-14 17:38:19 -0600

    final push

--- config/orm.js
+++ config/orm.js
@@ -25,36 +25,61 @@ function objToSql(ob) {
 
 // for sql functions
 var orm = {
-    // for everything
-    all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-        
-        // add connection.query
-        // dont forget to throw error
-        connection.query(queryString, function(err, result) {
-            if(err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
+        // for everything
+all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
 
-    // create function -- creates the table for the the values
-    create: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ")";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks();
-        queryString += ") ";
-    },
+            // add connection.query
+            // dont forget to throw error
+            connection.query(queryString, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                cb(result);
+            });
+        },
 
-    // updates the table with new info
-    update: function(table, objColValues, condition, cb) {
-        var queryString = "UPDATE " + table;
+        // create function -- creates the table for the the values
+        create: function (table, cols, vals, cb) {
+            var queryString = "INSERT INTO " + table;
+            queryString += " (";
+            queryString += cols.toString();
+            queryString += ")";
+            queryString += "VALUES (";
+            queryString += printQuestionMarks();
+            queryString += ") ";
 
-        // add query string += ...
-    }
-};
+            console.log(queryString);
+
+            //connection.query 
+            connection.query(queryString, vals, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                cb(result);
+            });
+        },
+
+        // updates the table with new info
+        update: function (table, objColValues, condition, cb) {
+            var queryString = "UPDATE " + table;
+
+            // add query string += ...
+            queryString += " SET ";
+            queryString += objToSql(objColVals);
+            queryString += " WHERE ";
+            queryString += condition;
+
+            console.log(queryString);
+
+            // connection.query
+            connection.query(queryString, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                cb(result);
+            });
+        }
+    };
+    module.exports = orm;
